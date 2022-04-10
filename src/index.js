41adab7471644276b5c2b200d61d82dfcc23fd80
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-04-10 16:43:16 +0400

    JSX Feature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//----------------------------------------------------------------------
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//JSX allows us to insert html in JS and inject JS inside the
+//html content as well, using the curly brackets -> {} <-
+//however, we can not write JS statements like: if-else or Functions
+
+const fName = "Taha";
+const lName = "Ahmed";
+const luckyNumber = Math.floor(Math.random() * 10);
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!{" "}
+    </h1>
+    <p>Your Lucky Number is {luckyNumber}.</p>
+  </div>,
+  document.getElementById("root")
+);
